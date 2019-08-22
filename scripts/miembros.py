fdc553804dbdb294e943354e2e00cf392712259d
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-08-22 12:13:14 -0300

    agregue algunas configuraciones básicas para el proyecto

--- scripts/miembros.py
+++ scripts/miembros.py
@@ -9,5 +9,13 @@ if __name__ == '__main__':
     KEY = os.environ.get('KEY')
     print(f'ingresando a redmine usando {KEY}')
     redmine = Redmine('https://pedidos.econo.unlp.edu.ar', key=KEY)
+
     for p in redmine.project.all():
-        print(p)
+        try:
+            if p.parent is None:
+                print(f'obtieniendo los miembros de proyecto: {p.id}')
+                memberships = redmine.project_membership.filter(project_id=p.id)
+                for m in memberships:
+                    print(m)
+        except Exception as a:
+            pass
